bddf1b41531b2e17ec362eafc55af93b6bf5bb7a
Even Torbjørnsen <591118@stud.hvl.no>
2023-03-05 21:09:24 +0100

    videre, se om fungerer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,25 @@ class HomeController < ApplicationController
     @message = "Hello kaken"
   end
 
+  def opprett
+    tekst = tekst.new(text_params)
+
+    if @tekst.save
+      redirect "/siste"
+    else
+      render '/homepage'
+    end
+  end
+
+  private
+    def text_params
+      params.require(:tekst).permit(:content)
+    end
+
+  end
+
   def siste
-    @text = Tekst.find(params[:id])
+    @tekst = Tekst.find(params[:id])
     @message = "Siste page av appen"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'home#homepage'
   
   get "/neste", to: 'home#neste'
+  post "/neste", to: "home#opprett"
+  
   get "/siste", to: "home#siste"
 
 end
